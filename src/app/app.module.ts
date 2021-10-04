dc7fb3052cca63018e82cf5462b9f6c1eea11c72
Erik Pedro <erikpedro@gmail.com>
2021-10-04 12:16:43 -0300

    Formulario de contato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContatoComponent } from './contato/contato.component';
+import { ContatoService } from './contato.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [ContatoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
